228d050a0d54e9d436b8585c0b33e4f4be03d890
Denis Kopylov <inedden@rambler.ru>
2024-07-08 11:02:12 +0500

    add func

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,19 @@
-def filter_by_state():
-    pass
+def filter_by_state(user_list: list, state: str = "EXECUTED") -> list:
+    """
+    Функция возвращает новый список словарей,
+    содержащий только те словари, у которых ключ
+    state соответствует указанному значению.
+    """
+    filtered_list = list()
+    for i in range(len(user_list)):
+        if user_list[i]["state"] == state:
+            filtered_list.append(user_list[i])
+    return filtered_list
+
+
+if __name__ == "__main__":
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+                          ))
