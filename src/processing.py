5ecd35db653c03817609be77ae9e791a3173482d
Denis Kopylov <inedden@rambler.ru>
2024-07-09 12:16:49 +0500

    back to first PR

--- src/processing.py
+++ src/processing.py
@@ -5,19 +5,19 @@ def filter_by_state(user_list: list, state: str = "EXECUTED") -> list:
     state соответствует указанному значению
     """
     filtered_list = list()
-    for index in range(len(user_list)):
-        if user_list[index]["state"] == state:
-            filtered_list.append(user_list[index])
+    for i in range(len(user_list)):
+        if user_list[i]["state"] == state:
+            filtered_list.append(user_list[i])
     return filtered_list
 
 
-def sort_by_date(user_list: list, sort_filter: bool = False) -> list:
+def sort_by_date(user_list: list, sort_filter: bool = True) -> list:
     """
     Функция, которая возвращает новый список, отсортированный по дате
     """
     sorted_list_date = sorted(user_list,
                               key=lambda x: x["date"],
-                              reverse=not sort_filter)
+                              reverse=sort_filter)
     return sorted_list_date
 
 
